063562c8d98c4d2338378d02867da3acb3ccd056
Rohit <senr32245@gmail.com>
2024-12-04 19:13:17 +0530

    Set AdminUserData

--- db/migrate/20241204124129_add_hotel_data.rb
+++ db/migrate/20241204124129_add_hotel_data.rb
@@ -1,13 +1,13 @@
 class AddHotelData < ActiveRecord::Migration[7.2]
   def change
     Hotel.create([
-    {
+  {
     name: "Ocean View",
     address: "456/2, vijay nagar indore, near opollo tawer",
     number: "9131433212",
     description: "A beautiful ocean-facing hotel.",
-    rating: 4
-    }
-    ]) 
-    end
+    rating: 4,
+  }
+])
+  end
 end
