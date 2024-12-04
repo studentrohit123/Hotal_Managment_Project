7494f469421dafa61baffeb68d5753d7ac9edb62
Rohit <senr32245@gmail.com>
2024-12-04 17:39:49 +0530

    create data on render through seeb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,18 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+puts "Seeding hotels..."
+
+Hotel.create([
+  {
+    name: "Ocean View",
+    address: "456/2, vijay nagar indore, near opollo tawer",
+    number: "9131433212",
+    description: "A beautiful ocean-facing hotel.",
+    rating: 4,
+    image: Rails.root.join('app', 'assets', 'images', 'imageH28_3.jpeg').open
+  }
+])
+
+puts "Seeding completed!"
